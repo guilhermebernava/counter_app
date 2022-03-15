f59724bbd16da8fabf297b280f47356287b0fd8e
Guilherme <36882198+guilhermebernava@users.noreply.github.com>
2022-03-15 10:30:44 -0300

    Update Home.dart

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -2,11 +2,15 @@
 
 import 'package:flutter/material.dart';
 
+//classe inicial
 class Home extends StatefulWidget {
   @override
+  //STATE sendo criado a partir da classe base e chamando
+  //a classe que extend esse estado.
   State<Home> createState() => _HomeState();
 }
 
+//classe criada que extende o STATE<HOME>
 class _HomeState extends State<Home> {
   var number = 0;
 
@@ -19,7 +23,9 @@ class _HomeState extends State<Home> {
     number--;
     setState(() {});
   }
-
+  
+  //LOGICA QUE CRIA O WIDGET
+  //quando for um STATELESS so vai existir esse CONSTRUTOR de BUILD
   @override
   Widget build(BuildContext context) {
     return Scaffold(
